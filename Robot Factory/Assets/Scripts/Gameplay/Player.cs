cf3683e7d202ca1438c38d4e36295d9d392308a5
KaiIC5343 <jacob.hyland@ontariotechu.net>
2025-11-11 20:54:00 -0500

    player health drop

---
+++ Robot Factory/Assets/Scripts/Gameplay/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 3;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "redEnemy" || collision.gameObject.tag == "blueEnemy" || collision.gameObject.tag == "greenEnemy" || collision.gameObject.tag == "yellowEnemy")
+        {
+            health -= 1;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+
+        }
+    }
+}
